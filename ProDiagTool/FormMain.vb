8025acda5dd9940ab222291b16a3400e9801c32c
Dieter Poesl <dieter.poesl@outlook.de>
2019-09-15 15:03:32 +0200

    MIT-License

--- ProDiagTool/FormMain.vb
+++ ProDiagTool/FormMain.vb
@@ -217,7 +217,8 @@ Public Class FormMain
                 For Each rowSymbol In dsSymbol.Tables("[PLC Tags$]").Rows
                     'Bei Supervised tag müssen die Anführungszeichen Chr(34) entfernt werden  
                     If rowProDiag.Item("Supervised tag").ToString.Replace(Chr(34), "") = rowSymbol.Item("Name").ToString Then
-                        rowProDiag.Item("Adresse") = rowSymbol.Item("Logical Address")
+                        'Bei Logical Adress wird das % entfernt
+                        rowProDiag.Item("Adresse") = rowSymbol.Item("Logical Address").ToString.Replace("%", "")
                         rowProDiag.Item("Comment") = rowSymbol.Item("Comment")
                         Anzahl += 1
                     End If
